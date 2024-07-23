ec3202c50c969babb78720e15b227564f5c7b0be
Jodi Kris Antoni <jody201542@gmail.com>
2024-07-23 19:08:30 +0700

    file baru

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git test</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error repellat non debitis vero sunt alias provident commodi ab. Neque ea consequatur officiis facilis doloribus aliquam harum consequuntur nulla illo. Vero.</p>
+</body>
+</html>
